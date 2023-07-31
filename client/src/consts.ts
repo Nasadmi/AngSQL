085e47564abf91e77438cd120c8f62b10c045fb9
Nasadmi <samueldiego828@gmail.com>
2023-07-31 23:00:23 +0200

    Add styles function for Alerts

--- client/src/consts.ts
+++ client/src/consts.ts
@@ -1,1 +1,8 @@
 export const SERVER_HOST = 'http://localhost:7777';
+
+export function ALERT_STYLE(background: string, color: string) {
+    return {
+        background: background,
+        color: color
+    };
+}

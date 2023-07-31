ae7aa6e815740d67e9211ffe1b18a9391f2c190c
Nasadmi <samueldiego828@gmail.com>
2023-07-31 14:55:19 +0200

    Add new type for post method

--- client/src/services/http.service.ts
+++ client/src/services/http.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 
 import { HttpClient } from "@angular/common/http";
-import { Connections } from "src/models/connections.model";
+import { Connections, Root } from "src/models/connections.model";
 
 @Injectable({
     providedIn: 'root'
@@ -14,7 +14,7 @@ export class HttpService {
         return this.http.get<typeof type>(url);
     }
 
-    post({ url, type, body }: { url: string; type: Connections[]; body: Connections | undefined; }) {
+    post({ url, type, body }: { url: string; type: Connections[] | Root['connectionsInterfaces']; body: Connections | undefined; }) {
         return this.http.post<typeof type>(url, body);
     }
 }

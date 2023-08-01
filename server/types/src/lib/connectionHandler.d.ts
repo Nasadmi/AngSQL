f4560d8cbbe3c77d52503439c221a37551d1e43d
Nasadmi <samueldiego828@gmail.com>
2023-08-01 18:18:26 +0200

    Update types

---
+++ server/types/src/lib/connectionHandler.d.ts
@@ -0,0 +1,3 @@
+import { ConnectionsStruct } from "../../config/types";
+export declare function addConnection(connection: ConnectionsStruct): Promise<void>;
+export declare function removeConnection(connection: ConnectionsStruct): Promise<void>;
---
+++ server/types/src/lib/crypt.d.ts
@@ -0,0 +1,5 @@
+export declare function encrypt(data: string): {
+    data: string;
+    key: string;
+};
+export declare function decrypt(data: string, key: string): string;

f7a705210be04245723cc084634e85d3a43135c5
Nasadmi <samueldiego828@gmail.com>
2023-08-01 14:27:41 +0200

    Update connection list component

--- client/src/app/connections-list/connections-list.component.ts
+++ client/src/app/connections-list/connections-list.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { SERVER_HOST } from 'src/consts';
 
 import { Connections } from 'src/models/connections.model';
+
+import { HttpService } from 'src/services/http.service';
+
+import * as alerts from "sweetalert2"
 
 @Component({
   selector: 'app-connections-list',
@@ -9,4 +14,70 @@ import { Connections } from 'src/models/connections.model';
 })
 export class ConnectionsListComponent {
   @Input() connections!: Connections;
+  constructor(private httpService: HttpService) { }
+  deleteConnection({
+    host,
+    port,
+    user,
+    database
+  }: Connections ) {
+    alerts.default.fire({
+      title: 'Are you sure?',
+      text: "If you want to delete this connection, please enter password.",
+      input: 'password',
+      icon: 'warning',
+      showCancelButton: true,
+      showConfirmButton: true,
+      color: 'var(--angsql-white)',
+      background: '#111'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const data: Connections = {
+          host, port, user, database
+        }
+        this.httpService.post({
+          url: `${SERVER_HOST}/api/connections/delete`,
+          type: data,
+          body: {
+            host: data.host,
+            port: data.port,
+            user: data.user,
+            database: data.database,
+            password: result.value
+          }
+        }).subscribe((res) => {
+            const response = res as Connections;
+            if (response.message === false) {
+              return alerts.default.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Something went wrong. Please try again later.',
+                color: 'var(--angsql-white)',
+                background: '#111'
+              })
+            } 
+            if (response.message === 'Password') {
+              return alerts.default.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Incorrect password.',
+                color: 'var(--angsql-white)',
+                background: '#111'
+              })
+            }
+            return alerts.default.fire({
+              icon: 'info',
+              title: 'Connection has been deleted.',
+              text: '',
+              timer: 10000,
+              showConfirmButton: true,
+              color: 'var(--angsql-white)',
+              background: '#111'
+            }).then(() => {
+              window.location.reload()
+            })
+        })
+      }
+    })
+  }
 }

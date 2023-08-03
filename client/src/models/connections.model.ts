a8a73da6e1e877b7c646c2a8841b4039b7afd323
Nasadmi <98781576+Nasadmi@users.noreply.github.com>
2023-08-03 19:48:07 +0200

    Update models

--- client/src/models/connections.model.ts
+++ client/src/models/connections.model.ts
@@ -3,12 +3,19 @@ export interface Root {
     connections: Connections[]
 }
 
-export interface Connections {
+interface Response {
+    error?: boolean;
+    message?: string | boolean
+}
+
+interface OtherData {
+    passwordExample?: string,
+}
+
+export interface Connections extends Response, OtherData{
     host: string;
     port: number | string | null;
     user: string;
     database?: string;
     password?: string;
-    passwordExample?: string;
-    message?: string | boolean;
 }

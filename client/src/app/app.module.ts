b2e60a9740f2b6350896fbb2b498f884c95921fd
Nasadmi <98781576+Nasadmi@users.noreply.github.com>
2023-08-04 14:05:55 +0200

    Update code order

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ConnectionsListComponent } from './connections-list/connections-list.component';
-import { AddConnectionComponent } from './add-connection/add-connection.component';
-import { EditConnectionComponent } from './edit-connection/edit-connection.component';
+import { ConnectionsListComponent } from './connectionManageComponents/connections-list/connections-list.component';
+import { AddConnectionComponent } from './connectionManageComponents/add-connection/add-connection.component';
+import { EditConnectionComponent } from './connectionManageComponents/edit-connection/edit-connection.component';
 
 @NgModule({
   declarations: [

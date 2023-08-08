5b8eaec444301cbf37b4c53b050641c8bdd15550
Nasadmi <98781576+Nasadmi@users.noreply.github.com>
2023-08-08 20:06:51 +0200

    Delete canActive

--- client/src/guards/auth.guard.ts
+++ client/src/guards/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, UrlTree, Router } from "@angular/router";
+import { UrlTree, Router } from "@angular/router";
 import { AuthService } from "src/services/auth.service";
 import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard {
     constructor(private authService: AuthService, private router: Router) { }
 
     canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {

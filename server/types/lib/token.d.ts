f3d29172ddc002846a82bc9e8b8fd4856688ba9b
Nasadmi <98781576+Nasadmi@users.noreply.github.com>
2023-08-08 23:32:31 +0200

    to

--- server/types/lib/token.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-import * as jwt from 'jsonwebtoken';
-export declare function generateToken(payload: string | object | Buffer, key: string): string;
-export declare function verifyToken(token: string, key: string): string | jwt.JwtPayload;
-export declare function decodeToken(token: string): string | jwt.JwtPayload | null;

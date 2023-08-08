023262764271df27f4a8208ccd8a772e5be71a4b
Nasadmi <98781576+Nasadmi@users.noreply.github.com>
2023-08-08 19:57:14 +0200

    Update routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './connectionRoute/homeComponent/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthGuard } from 'src/guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'connection',
+    component: HomeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '404',
+    component: NotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

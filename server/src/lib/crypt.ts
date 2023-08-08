7428bdc47140e923140c06594130c307575c5f5f
Nasadmi <98781576+Nasadmi@users.noreply.github.com>
2023-08-08 19:52:22 +0200

    Update crypt

--- server/src/lib/crypt.ts
+++ server/src/lib/crypt.ts
@@ -1,6 +1,6 @@
 import crypto from 'crypto-js';
 
-function getRandomString(length: number) {
+export function getRandomString(length: number) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/@'
     let result = ''
     for (let i = 0; i < length; i++) {

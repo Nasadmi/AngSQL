430e20e31f971dd81c8235c05fd5266bc4456853
Nasadmi <98781576+Nasadmi@users.noreply.github.com>
2023-08-10 02:40:45 +0200

    Add mysql control API

---
+++ server/src/router/db.ts
@@ -0,0 +1,44 @@
+import { Router } from "express";
+
+import { db } from "./connections";
+
+import { MySQLResponseDatabases } from "../config/types";
+
+import * as JWT from "jsonwebtoken";
+
+const router = Router();
+
+router.get("/api/db/databases", async (req, res) => {
+    const verifyPower = {
+        token: req.header('X-Authorization-Token') as string,
+        key: req.header('X-Authization-Key') as string
+    }
+    try {
+        JWT.verify(verifyPower.token, verifyPower.key);
+        db.query("SHOW DATABASES", (err, result) => {
+            const arr: string[] = []; 
+            if (err) {
+                res.json({
+                    err: true,
+                    message: err.message
+                });
+            } else {
+                const databaseList = result as MySQLResponseDatabases[]
+                databaseList.forEach(database => {
+                    arr.push(database.Database)
+                })
+                res.json({
+                    err: false,
+                    data: arr
+                });
+            }
+        });
+    } catch (error) {
+        res.json({
+            err: true,
+            message: "Invalid token or Invalid query"
+        });
+    }
+})
+
+export { router as dbAPI };
